bdb1124c6305c26fc2312540bc9e348cf9c6966c
kooooohe <1211lta+github@gmail.com>
2020-04-01 16:48:40 +0900

    network

---
+++ main/network.tf
@@ -0,0 +1,76 @@
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+      Name = "example"
+  }
+}
+
+/*public*/
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id 
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+/*public*/
+
+/*private*/
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.64.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+/*private*/
+
+/*NAT*/
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id = aws_subnet.public.id
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_route" "private" {
+  route_table_id = aws_route_table.private.id 
+  nat_gateway_id = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+/*NAT*/
