5d0ec7902a8a1390890102239d3d1430d3a95795
kooooohe <1211lta+github@gmail.com>
2020-04-15 15:13:44 +0900

    refactoring

--- main/dns.tf
+++ main/dns.tf
@@ -2,15 +2,15 @@ data "aws_route53_zone" "example" {
     name = "0x0-dev.co.uk"
 }
 
-resource "aws_route53_zone" "example_test" {
+resource "aws_route53_zone" "example_sub" {
   name = "test.0x0-dev.co.uk"
   tags = {
       Name = "terraform_example"
   }
 }
 
-resource "aws_route53_record" "example_additional" {
-  depends_on = [aws_route53_zone.example_test]
+resource "aws_route53_record" "example" {
+  depends_on = [aws_route53_zone.example_sub]
   allow_overwrite = true
   name            = "test.0x0-dev.co.uk"
   ttl             = 30
@@ -18,18 +18,18 @@ resource "aws_route53_record" "example_additional" {
   zone_id         = data.aws_route53_zone.example.zone_id
 
   records = [
-    aws_route53_zone.example_test.name_servers.0,
-    aws_route53_zone.example_test.name_servers.1,
-    aws_route53_zone.example_test.name_servers.2,
-    aws_route53_zone.example_test.name_servers.3,
+    aws_route53_zone.example_sub.name_servers.0,
+    aws_route53_zone.example_sub.name_servers.1,
+    aws_route53_zone.example_sub.name_servers.2,
+    aws_route53_zone.example_sub.name_servers.3,
   ]
 }
 
-resource "aws_route53_record" "example" {
+resource "aws_route53_record" "example_sub" {
   //zone_id = data.aws_route53_zone.example.zone_id
   //name = data.aws_route53_zone.example.name
-  zone_id = aws_route53_zone.example_test.zone_id
-  name = aws_route53_zone.example_test.name
+  zone_id = aws_route53_zone.example_sub.zone_id
+  name = aws_route53_zone.example_sub.name
   type = "A"
 
   alias {
@@ -40,5 +40,27 @@ resource "aws_route53_record" "example" {
 }
 
 output "domain_name" {
-    value = aws_route53_record.example.name
+  value = aws_route53_record.example_sub.name
 }
+
+/*ACM*/
+
+/*
+resource "aws_acm_certificate" "example" {
+  domain_name = aws_route53_record.example.name
+  subject_alternative_names = [aws_route53_record.example_sub.name]
+  validation_method = "DNS"
+
+  lifecycle {
+      create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "example_certificate" {
+  name = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+  type = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+  records =[aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+  zone_id = data.aws_route53_zone.example.id
+  ttl = 60
+}
+*/
