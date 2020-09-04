c2a994a49ab1620ef95a71ba5ee4740d945b179c
kooooohe <1211lta+github@gmail.com>
2020-09-04 14:56:22 +0900

    made sending logs of ECS to Cloud Watch

--- main/ecs.tf
+++ main/ecs.tf
@@ -9,6 +9,8 @@ resource "aws_ecs_task_definition" "example" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   container_definitions    = file("./container_definitions.json")
+
+  execution_role_arn = module.ecs_task_excution_role.iam_role_arn
 }
 
 resource "aws_ecs_service" "example" {
@@ -47,4 +49,26 @@ module "nginx_sg" {
   vpc_id      = aws_vpc.example.id
   port        = 80
   cidr_blocks = [aws_vpc.example.cidr_block]
+}
+
+
+data "aws_iam_policy" "ecs_task_excution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_json = data.aws_iam_policy.ecs_task_excution_role_policy.policy
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
+
+module "ecs_task_excution_role" {
+  source     = "./iam_role"
+  name       = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ecs_task_execution.json
 }
